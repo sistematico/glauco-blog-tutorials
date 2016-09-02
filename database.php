5d8e4edb06982fa2f21a3e6e201bda6ba2fdf5f7
Lucas Saliés Brum <lucas@archlinux.com.br>
2016-09-01 22:08:11 -0400

    Alterado o DSN para lidar com SQLite ao invés de MySQL

--- database.php
+++ database.php
@@ -4,7 +4,7 @@ session_start();
 
 try{
     // Faz conexão com banco de daddos
-    $pdo = new PDO("mysql:host=127.0.0.1;dbname=glauco_blog_tutorials_db;", "root", "");
+    $pdo = new PDO("sqlite:users.db");
 }catch(PDOException $e){
     // Caso ocorra algum erro na conexão com o banco, exibe a mensagem
     echo 'Falha ao conectar no banco de dados: '.$e->getMessage();
